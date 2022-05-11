16246748c9d48cd2bef22430f21d6a2a7b596bc7
Mikhail <mikhail@volkovlabs.io>
2022-05-11 16:22:14 -0400

    Add ability to move parameters up and down

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,17 @@
+import { InputParameter } from './types';
+
 /**
  * Capitalize First Letter
  */
 export const CapitalizeFirstLetter = (str: string) => {
   return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+/**
+ * Move Input Parameters
+ */
+export const MoveInputParameters = (array: InputParameter[], from: number, to: number) => {
+  const element = array[from];
+  array.splice(from, 1);
+  array.splice(to, 0, element);
 };
