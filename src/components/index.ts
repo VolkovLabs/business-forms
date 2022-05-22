f9a2c1964840a1cce901b3d733cf798b48d64cb3
Mikhail <mikhail@volkovlabs.io>
2022-05-21 22:14:55 -0400

    Add Layout Sections Editor

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export * from './FormElements';
 export * from './FormElementsEditor';
 export * from './FormPanel';
 export * from './HeaderParametersEditor';
+export * from './LayoutSectionsEditor';
