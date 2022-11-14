ef46f4578368c845be91226a28edd170de3e7952
Mikhail <mikhail@volkovlabs.io>
2022-11-14 13:40:59 -0500

    Fix Password and Boolean elements confirmation

--- src/components/FormPanel/FormPanel.tsx
+++ src/components/FormPanel/FormPanel.tsx
@@ -467,6 +467,19 @@ export const FormPanel: React.FC<Props> = ({
                   }
 
                   /**
+                   * Skip Password elements
+                   */
+                  if (element.type === FormElementType.PASSWORD) {
+                    return (
+                      <tr className={styles.confirmTable} key={element.id}>
+                        <td className={styles.confirmTableTd}>{element.title || element.tooltip}</td>
+                        <td className={styles.confirmTableTd}>*********</td>
+                        <td className={styles.confirmTableTd}>*********</td>
+                      </tr>
+                    );
+                  }
+
+                  /**
                    * Convert DateTime object to ISO string
                    */
                   if (element.type === FormElementType.DATETIME) {
@@ -476,8 +489,12 @@ export const FormPanel: React.FC<Props> = ({
                   return (
                     <tr className={styles.confirmTable} key={element.id}>
                       <td className={styles.confirmTableTd}>{element.title || element.tooltip}</td>
-                      <td className={styles.confirmTableTd}>{initial[element.id]}</td>
-                      <td className={styles.confirmTableTd}>{element.value}</td>
+                      <td className={styles.confirmTableTd}>
+                        {initial[element.id] === undefined ? '' : String(initial[element.id])}
+                      </td>
+                      <td className={styles.confirmTableTd}>
+                        {element.value === undefined ? '' : String(element.value)}
+                      </td>
                     </tr>
                   );
                 })}
