27f2d53b0ff4c0dcad9abc8f281c175b49329666
Mikhail <mikhail@volkovlabs.io>
2022-12-19 19:41:55 -0500

    Add "Buttons only" Layout to do GET/POST/DELETE requests

--- src/constants/layout.ts
+++ src/constants/layout.ts
@@ -4,6 +4,7 @@ import { SelectableValue } from '@grafana/data';
  * Layout Variants
  */
 export const enum LayoutVariant {
+  NONE = 'none',
   SINGLE = 'single',
   SPLIT = 'split',
 }
@@ -14,10 +15,17 @@ export const enum LayoutVariant {
 export const LayoutVariantOptions: SelectableValue[] = [
   {
     value: LayoutVariant.SINGLE,
+    description: 'All elements together.',
     label: 'Basic',
   },
   {
+    value: LayoutVariant.NONE,
+    description: 'Buttons only.',
+    label: 'Buttons only',
+  },
+  {
     value: LayoutVariant.SPLIT,
+    description: 'Elements split in separate sections.',
     label: 'Sections',
   },
 ];
