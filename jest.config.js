300ddcae2bf2bab3ac097afaa998fef11bddeea3
Mikhail <mikhail@volkovlabs.io>
2023-07-03 21:51:47 -0400

    Update Jest config

--- jest.config.js
+++ jest.config.js
@@ -2,29 +2,7 @@
 // generally used by snapshots, but can affect specific tests
 process.env.TZ = 'UTC';
 
-const defaultConfig = require('./.config/jest.config');
-
 module.exports = {
   // Jest configuration provided by Grafana scaffolding
-  ...defaultConfig,
-  transform: {
-    ...defaultConfig.transform,
-    '^.+\\.(t|j)sx?$': [
-      '@swc/jest',
-      {
-        /**
-         * Override sourceMaps with inline to see the correct coverage lines
-         */
-        sourceMaps: 'inline',
-        jsc: {
-          parser: {
-            syntax: 'typescript',
-            tsx: true,
-            decorators: false,
-            dynamicImport: true,
-          },
-        },
-      },
-    ],
-  },
+  ...require('./.config/jest.config'),
 };
