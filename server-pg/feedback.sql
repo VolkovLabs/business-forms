5e6031f0ef9900b74a87f6c7ed822be8e26e21c3
Mikhail <mikhail@volkovlabs.io>
2023-08-29 22:01:49 -0400

    Update Query and Data Source request

--- server-pg/feedback.sql
+++ server-pg/feedback.sql
@@ -6,3 +6,15 @@ CREATE TABLE feedbacks (
     description text,
     type text
 );
+
+CREATE TABLE configuration (
+    name text,
+    max integer not null,
+    min integer not null,
+    speed integer not null
+);
+
+insert into configuration values ('device1', 100, 10, 54);
+insert into configuration values ('device2', 60, 0, 10);
+insert into configuration values ('device3', 60, 30, 40);
+insert into configuration values ('device4', 34, 10, 20);
