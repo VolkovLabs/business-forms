3d016f47217c484eff22925706f32009df55e451
asimonok <sashasimonok@gmail.com>
2024-02-29 14:40:45 +0300

    Add using code parameters builder from @volkovlabs/components

--- src/utils/code-parameters.ts
+++ src/utils/code-parameters.ts
@@ -1,127 +1,15 @@
 import { AlertPayload, EventBus, InterpolateFunction, PanelData } from '@grafana/data';
 import { BackendSrv, FetchResponse, LocationService, TemplateSrv, toDataQueryResponse } from '@grafana/runtime';
-import { CodeEditorSuggestionItem, CodeEditorSuggestionItemKind } from '@grafana/ui';
+import { CodeEditorSuggestionItemKind } from '@grafana/ui';
+import { CodeParameterItem, CodeParametersBuilder } from '@volkovlabs/components';
 
 import { FormElement, LocalFormElement, PanelOptions } from '../types';
 import { fileToBase64 } from './request';
 
 /**
- * Code Parameter Group
- */
-interface CodeParameterGroup {
-  /**
-   * Detail
-   *
-   * @type {string}
-   */
-  detail: string;
-
-  /**
-   * Items
-   */
-  items: Record<string, CodeParameterGroup | CodeParameterItem>;
-}
-
-/**
- * Parameter Item
- */
-export class CodeParameterItem<TValue = unknown> {
-  /**
-   * Specify for type validation
-   */
-  value: TValue = {} as TValue;
-
-  constructor(
-    public detail: string,
-    public kind: CodeEditorSuggestionItemKind = CodeEditorSuggestionItemKind.Property
-  ) {}
-}
-
-/**
- * Convert group to payload object
- */
-type PayloadForGroup<TGroup extends CodeParameterGroup> = {
-  [Key in keyof TGroup['items']]: TGroup['items'][Key] extends CodeParameterGroup
-    ? PayloadForGroup<TGroup['items'][Key]>
-    : TGroup['items'][Key] extends CodeParameterItem
-      ? TGroup['items'][Key]['value']
-      : unknown;
-};
-
-/**
- * Code Parameters
- */
-export class CodeParameters<TGroup extends CodeParameterGroup> {
-  /**
-   * Built Suggestions based on config
-   */
-  suggestions: CodeEditorSuggestionItem[] = [];
-
-  constructor(group: TGroup, basePath = 'context') {
-    /**
-     * Add base suggestion
-     */
-    this.suggestions.push({
-      label: basePath,
-      kind: CodeEditorSuggestionItemKind.Constant,
-      detail: group.detail,
-    });
-
-    /**
-     * Add all suggestions
-     */
-    this.addSuggestions(basePath, group);
-  }
-
-  /**
-   * Add suggestions
-   * @param path
-   * @param group
-   * @private
-   */
-  private addSuggestions(path: string, group: CodeParameterGroup) {
-    Object.entries(group.items).forEach(([key, item]) => {
-      const itemPath = `${path}.${key}`;
-
-      /**
-       * Group
-       */
-      if ('items' in item) {
-        this.suggestions.push({
-          label: itemPath,
-          detail: item.detail,
-          kind: CodeEditorSuggestionItemKind.Property,
-        });
-
-        this.addSuggestions(itemPath, item);
-
-        return;
-      }
-
-      /**
-       * Item
-       */
-      this.suggestions.push({
-        label: itemPath,
-        detail: item.detail,
-        kind: item.kind,
-      });
-    });
-  }
-
-  /**
-   * Create payload method for type validation
-   * @param payload
-   */
-  create(payload: PayloadForGroup<TGroup>) {
-    return payload;
-  }
-}
-
-/**
  * Request Code Parameters
  */
-export const requestCodeParameters = new CodeParameters({
+export const requestCodeParameters = new CodeParametersBuilder({
   detail: 'All passed possible properties and methods.',
   items: {
     panel: {
@@ -194,7 +82,7 @@ export const requestCodeParameters = new CodeParameters({
 /**
  * Element Value Changed Code Parameters
  */
-export const elementValueChangedCodeParameters = new CodeParameters({
+export const elementValueChangedCodeParameters = new CodeParametersBuilder({
   detail: 'All passed possible properties and methods.',
   items: {
     panel: {
@@ -268,7 +156,7 @@ export const elementValueChangedCodeParameters = new CodeParameters({
 /**
  * Show If Code Parameters
  */
-export const showIfCodeParameters = new CodeParameters({
+export const showIfCodeParameters = new CodeParametersBuilder({
   detail: 'All passed possible properties and methods.',
   items: {
     panel: {
@@ -292,7 +180,7 @@ export const showIfCodeParameters = new CodeParameters({
 /**
  * Disable If Code Parameters
  */
-export const disableIfCodeParameters = new CodeParameters({
+export const disableIfCodeParameters = new CodeParametersBuilder({
   detail: 'All passed possible properties and methods.',
   items: {
     panel: {
@@ -316,7 +204,7 @@ export const disableIfCodeParameters = new CodeParameters({
 /**
  * Get Options Code Parameters
  */
-export const getOptionsCodeParameters = new CodeParameters({
+export const getOptionsCodeParameters = new CodeParametersBuilder({
   detail: 'All passed possible properties and methods.',
   items: {
     panel: {
@@ -341,7 +229,7 @@ export const getOptionsCodeParameters = new CodeParameters({
 /**
  * Get Payload Code Parameters
  */
-export const getPayloadCodeParameters = new CodeParameters({
+export const getPayloadCodeParameters = new CodeParametersBuilder({
   detail: 'All passed possible properties and methods.',
   items: {
     panel: {
