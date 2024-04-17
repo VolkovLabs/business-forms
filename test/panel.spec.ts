e146f5c0ed69bad8679f09da165d2085b97e5d15
vitPinchuk <vit.pinchuck@gmail.com>
2024-04-17 14:24:16 +0300

    Add plugin e2e tests and remove cypress

---
+++ test/panel.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from '@grafana/plugin-e2e';
+import { TEST_IDS } from '../src/constants/tests';
+
+test.describe('Data Manipulation Panel', () => {
+  test('Should display a Form', async ({ gotoDashboardPage, dashboardPage }) => {
+    /**
+     * Go To E2E dashboard
+     * return dashboardPage
+     */
+    await gotoDashboardPage({ uid: 'ddab9faf-2c15-431a-a047-9a7a6a0aed71' });
+
+    /**
+     * Find panel by title with chart
+     * Should be visible
+     */
+    await expect(dashboardPage.getPanelByTitle('Single Form').locator).toBeVisible();
+
+    /**
+     * Check and compare image
+     */
+    await expect(
+      dashboardPage.getPanelByTitle('Single Form').locator.getByTestId(TEST_IDS.panel.root)
+    ).toHaveScreenshot('actual-screenshot.png');
+  });
+});
