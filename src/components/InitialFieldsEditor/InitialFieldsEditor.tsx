82c5a4682c11b283355b6f4dcfbeead352f4262e
asimonok <sashasimonok@gmail.com>
2024-07-18 13:44:27 +0300

    Formatting

--- src/components/InitialFieldsEditor/InitialFieldsEditor.tsx
+++ src/components/InitialFieldsEditor/InitialFieldsEditor.tsx
@@ -23,6 +23,9 @@ export const InitialFieldsEditor: React.FC<Props> = ({ value, onChange, context 
     value,
   });
 
+  /**
+   * Query Fields
+   */
   const queryFields = useQueryFields({
     data: context.data,
     isEnabled: context.options?.initial?.method === RequestMethod.QUERY,
@@ -34,37 +37,35 @@ export const InitialFieldsEditor: React.FC<Props> = ({ value, onChange, context 
   return (
     <div data-testid={TEST_IDS.initialFieldsEditor.root}>
       {elements.map((element) => {
-        return context.options?.initial?.method === RequestMethod.DATASOURCE ? (
+        return (
           <InlineFieldRow key={element.uid}>
             <InlineField grow={true} label={element.title} labelWidth={14}>
-              <Input
-                value={element.fieldName || ''}
-                onChange={(event: ChangeEvent<HTMLInputElement>) => {
-                  onChangeElement({
-                    ...element,
-                    fieldName: event.target.value,
-                  });
-                }}
-                data-testid={TEST_IDS.initialFieldsEditor.fieldNamePicker}
-              />
-            </InlineField>
-          </InlineFieldRow>
-        ) : (
-          <InlineFieldRow key={element.uid}>
-            <InlineField grow={true} label={element.title} labelWidth={14}>
-              <Select
-                value={element.queryField?.value}
-                options={queryFields}
-                onChange={(item) => {
-                  onChangeElement({
-                    ...element,
-                    queryField: item,
-                  });
-                }}
-                aria-label={TEST_IDS.initialFieldsEditor.fieldFromQueryPicker}
-                data-testid={TEST_IDS.initialFieldsEditor.fieldFromQueryPicker}
-                isClearable={true}
-              />
+              {context.options?.initial?.method === RequestMethod.DATASOURCE ? (
+                <Input
+                  value={element.fieldName || ''}
+                  onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                    onChangeElement({
+                      ...element,
+                      fieldName: event.target.value,
+                    });
+                  }}
+                  data-testid={TEST_IDS.initialFieldsEditor.fieldNamePicker}
+                />
+              ) : (
+                <Select
+                  value={element.queryField?.value}
+                  options={queryFields}
+                  onChange={(item) => {
+                    onChangeElement({
+                      ...element,
+                      queryField: item,
+                    });
+                  }}
+                  aria-label={TEST_IDS.initialFieldsEditor.fieldFromQueryPicker}
+                  data-testid={TEST_IDS.initialFieldsEditor.fieldFromQueryPicker}
+                  isClearable={true}
+                />
+              )}
             </InlineField>
           </InlineFieldRow>
         );
